82f13477ac6eb47e1c385be9675fc1dee1c3e80a
hamdyaea <hamdy.aea@protonmail.com>
2023-05-22 20:33:32 +0200

    gabarits

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return render(request,
+        'bands/hello.html',
+        {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
